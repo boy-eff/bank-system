734288a6268e7a31830aecf79377c892f92d2737
Danila <danil.boev80@gmail.com>
2024-02-04 19:39:24 +0100

    Configure docker compose

--- BankSystem/Program.cs
+++ BankSystem/Program.cs
@@ -12,13 +12,11 @@ builder.Services.AddDbContext<BankDbContext>(options => options.UseNpgsql(config
 builder.Services.AddControllers();
 var app = builder.Build();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.MapControllers();
 
